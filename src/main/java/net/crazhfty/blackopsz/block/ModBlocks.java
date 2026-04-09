33bbad94c0735c6fb318df225cf6163db07d70dc
uporabnik <lunaxoxone2025@gmail.com>
2026-04-09 13:00:09 +0200

    trying to fix this things

--- src/main/java/net/crazhfty/blackopsz/block/ModBlocks.java
+++ src/main/java/net/crazhfty/blackopsz/block/ModBlocks.java
@@ -17,32 +17,26 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(BlackOpsZ.MOD_ID);
 
-
     public static final DeferredBlock<Block> AETHER_CRYSTAL_BLOCK = registerBlock("aether_crystal_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4.0f)
-                    .requiresCorrectToolForDrops()
+                    .noOcclusion()
                     .sound(SoundType.AMETHYST)));
 
     public static final DeferredBlock<Block> AETHER_CRYSTAL_ORE = registerBlock("aether_crystal_ore",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(3.0f)
-                    .requiresCorrectToolForDrops()
                     .sound(SoundType.STONE)));
 
     public static final DeferredBlock<Block> ELEMENTAL_115_ORE = registerBlock("elemental_115_ore",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(3.0f)
-                    .requiresCorrectToolForDrops()
                     .sound(SoundType.DEEPSLATE)));
 
     public static final DeferredBlock<Block> ELEMENTAL_115_BLOCK = registerBlock("elemental_115_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(3.0f)
-                    .requiresCorrectToolForDrops()
                     .sound(SoundType.NETHERITE_BLOCK)));
-
-
 
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
